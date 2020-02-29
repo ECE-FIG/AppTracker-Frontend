0f388578ccc0d15876c15acb94df4847cba0d2e0
Jianchen Gu <jianchen.gu3@gmail.com>
2020-02-29 17:11:28 -0600

    started on profile + history

--- vue-app/src/components/History.vue
+++ vue-app/src/components/History.vue
@@ -1,11 +1,57 @@
 <template>
   <v-content>
-    
+    <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
   </v-content>
 </template>
 
 <script>
-  
+  export default {
+    data: () => ({
+      headers: [
+          {
+            text: 'Company',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Name', value: 'calories' },
+          { text: 'Position', value: 'fat' },
+          { text: 'Location', value: 'carbs' },
+          { text: 'Description', value: 'protein' },
+          { text: 'Date', value: 'iron' },
+        ],
+      jobHistory: [
+        {
+        "Company": "Facebook", 
+        "Position": "Software Engineer",
+        "Location": "Austin, TX",
+        "Description": "Someone who can code"
+        },
+        {
+        "Company": "Amazon", 
+        "Position": "Software Engineer",
+        "Location": "Austin, TX",
+        "Description": "Someone who can code"
+        },
+        {
+        "Company": "Netflix", 
+        "Position": "Software Engineer",
+        "Location": "Austin, TX",
+        "Description": "Someone who can code"
+        },
+        {
+        "Company": "Google", 
+        "Position": "Software Engineer",
+        "Location": "Austin, TX",
+        "Description": "Someone who can code"
+        }]
+    })
+  };
 </script>
 
 <style>
