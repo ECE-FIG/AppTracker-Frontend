4354b1078f0fab0c1e63b1ab8654bd80132029e3
RickyMatsumoto <43117303+RickyMatsumoto@users.noreply.github.com>
2020-02-29 19:45:19 -0600

    just cuz

--- vue-app/src/components/Profile.vue
+++ vue-app/src/components/Profile.vue
@@ -1,14 +1,14 @@
 <template>
   <v-content>
-    <br />
-    <v-card class="mx-auto" width="600" height="120">
+    <v-container>
       <v-row>
-        <v-col align-self="center" class="pa-0" cols="12">
+        <v-col></v-col>
+        <v-col align-self="center" class="pa-0" cols="4">
           <v-avatar class="profile" color="grey" size="120">
             <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
           </v-avatar>
         </v-col>
-       <v-col>
+        <v-col>
           <v-list-item color="rgba(0, 0, 0, .4)" dark>
             <v-list-item-content>
               <v-list-item-title class="title">{{ profile.name }}</v-list-item-title>
@@ -16,9 +16,10 @@
               <v-list-item-subtitle>{{ profile.email }}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
-       </v-col>
+        </v-col>
+        <v-col></v-col>
       </v-row>
-    </v-card>
+    </v-container>
   </v-content>
 </template>
 
