1cff749c43516fb91abdb9742bdb444f36529747
anqing <benjamin.c0427@gmail.com>
2020-02-29 19:28:18 -0600

    axios

--- vue-app/src/components/Jobform.vue
+++ vue-app/src/components/Jobform.vue
@@ -1,24 +1,50 @@
 <template>
   <v-content>
     <div>
-    <v-col cols="12" sm="10">
-      <v-text-field label="Company Name" :rules="rules" hide-details="auto" width="500"></v-text-field>
-      <v-text-field label="Job Title" :rules="rules" width="500"></v-text-field>
-      <v-text-field label="Location" width="500"></v-text-field>
-      <v-text-field label="URL" width="500"></v-text-field>
-      <v-select :items="items" :rules="rules" label="Status" width="500"></v-select>
-      <v-btn color="cyan" dark >Submit</v-btn>
+      <v-col cols="12" sm="10">
+        <v-text-field label="Company Name" v-model="companyName" :rules="rules" hide-details="auto" width="500"></v-text-field>
+        <v-text-field label="Job Title" v-model="jobTitle" :rules="rules" width="500"></v-text-field>
+        <v-text-field label="Location" v-model="location" width="500"></v-text-field>
+        <v-text-field label="link" v-model="link" width="500"></v-text-field>
+        <v-select :items="items" v-model="status" :rules="rules" label="Status" width="500"></v-select>
+        <v-btn color="cyan" dark @click="submit">Submit</v-btn>
       </v-col>
     </div>
   </v-content>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data: () => ({
     rules: [value => !!value || "Required."],
-    items: ["Applied", "Interviewing", "Accepted", "Rejected"]
-  })
+    items: ["Applied", "Interviewing", "Accepted", "Rejected"],
+    companyName: "",
+    jobTitle: "",
+    location: "",
+    link: "",
+    status: ""
+  }),
+  methods: {
+    submit() {
+      console.log(this.link)
+      axios
+        .post("http://localhost:3000/applications", {
+          companyName: this.companyName,
+          jobTitle: this.jobTitle,
+          location: this.location,
+          link: this.link,
+          status: this.status
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
 };
 </script>
 
